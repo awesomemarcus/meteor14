769ea64400d4a2d8522d3ebc9cc2adf0f8063c73
Ricardo Pumanes <ric@snapzio.com>
2016-05-23 11:08:55 +0800

    added missing files

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@kadira/storybook';
+
+/* import css */
+import '../public/bootstrap-3.3.6/css/bootstrap.min.css';
+import '../client/assets/stylesheets/products.css';
+
+function loadStories() {
+  require('../client/modules/users/components/.stories/signup');
+  require('../client/modules/products/components/.stories/');
+  // require as many stories as you need.
+}
+
+configure(loadStories, module);
