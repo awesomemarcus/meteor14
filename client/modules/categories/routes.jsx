05b809cb803f3425fbeac7046477dbee54032786
rod ghost <rod@snapzio.com>
2016-05-23 16:24:51 +0800

    correct route with linting

--- client/modules/categories/routes.jsx
+++ client/modules/categories/routes.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 
-import AuthChecker from '/client/modules/users/containers/auth_checker.js';
-import MainLayout from '/client/modules/core/components/main_layout.jsx';
+import AuthChecker from '../users/containers/auth_checker.js';
+import MainLayout from '../core/components/main_layout.jsx';
 import CategoriesAdd from './containers/categories_add.js';
 import CategoriesUpdate from './containers/categories_update.js';
 import CategoriesView from './containers/categories_view.js';
@@ -19,45 +19,45 @@ export default function (injectDeps, {FlowRouter}) {
     name: 'categories_add',
     action() {
       mount(MainLayoutCtx, {
-        content: () => (<CategoriesAdd />)
+        content: () => (<CategoriesAdd />),
       });
-    }
+    },
   });
 
   FlowRouter.route('/categories/update/:category_id', {
     name: 'categories_update',
     action({category_id}) {
       mount(MainLayoutCtx, {
-        content: () => (<CategoriesUpdate category_id={category_id} />)
+        content: () => (<CategoriesUpdate category_id={category_id} />),
       });
-    }
+    },
   });
 
   FlowRouter.route('/categories/view/:category_id', {
     name: 'categories_view',
     action({category_id}) {
       mount(MainLayoutCtx, {
-        content: () => (<CategoriesView category_id={category_id} />)
+        content: () => (<CategoriesView category_id={category_id} />),
       });
-    }
+    },
   });
 
   FlowRouter.route('/categories/delete/:category_id', {
     name: 'categories_delete',
     action({category_id}) {
       mount(MainLayoutCtx, {
-        content: () => (<CategoriesDelete category_id={category_id} />)
+        content: () => (<CategoriesDelete category_id={category_id} />),
       });
-    }
+    },
   });
 
   FlowRouter.route('/categories/list', {
     name: 'categories_list',
     action() {
       mount(MainLayoutCtx, {
-        content: () => (<CategoriesList />)
+        content: () => (<CategoriesList />),
       });
-    }
+    },
   });
 
 
