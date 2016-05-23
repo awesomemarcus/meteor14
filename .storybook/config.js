068d045de96556cefc3e91ed777711122b21ef59
jay <jay@snapzio.com>
2016-05-23 13:56:48 +0800

    added stories for login component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,11 @@ import { configure } from '@kadira/storybook';
 
 /* import css */
 import '/public/bootstrap-3.3.6/css/bootstrap.min.css';
+import '/client/assets/stylesheets/custom_style.css';
 import '/client/assets/stylesheets/products.css';
 
 function loadStories() {
-  require('../client/modules/users/components/.stories/signup');
+  require('../client/modules/users/components/.stories/');
   require('../client/modules/products/components/.stories/');
   // require as many stories as you need.
 }
