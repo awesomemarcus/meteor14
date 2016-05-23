7041cb4702fa6c0a18ccbcc93c4a3834e5d1bd7c
Ricardo Pumanes <ric@snapzio.com>
2016-05-23 17:21:51 +0800

    added categories update functionality

--- client/modules/categories/components/categories_update.jsx
+++ client/modules/categories/components/categories_update.jsx
@@ -1,53 +1,38 @@
 import React from 'react';
 
-
-
-
 class CategoriesUpdate extends React.Component {
-  render() {
-    const {error} = this.props;
-    const {category} = this.props;
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const {categoriesUpdate} = this.props;
+    const {name} = this.refs;
+    categoriesUpdate(name.value);
+  }
+
+  render() {
+    const {nameError, category} = this.props;
 
     return (
-      <form onSubmit={this.PostsUpdate.bind(this)}>
-      <div className="form-group">
-        <h5>Category Add</h5>
-      </div>
-      <div className="form-group">
-        {error ? <p className="center bg-danger ">{error}</p> : null }
-      </div>
+      <div className="col-sm-4">
+      <form onSubmit={this.handleSubmit.bind(this)}>
+
+        <div className="form-group">
+          <h3>Category Update</h3>
+        </div>
 
         <div className="form-group">
           <label for="categoryName">Name</label>
           <input ref="name" type="text" className="form-control" id="categoryName" placeholder="Name" defaultValue={category.name} />
+          <p className="error text-error">{nameError ? nameError : ''}</p>
         </div>
 
-
-        <div className="form-group">
-          <label for="categoryDetails">Details</label>
-          <textarea ref="details"  type="textarea" className="form-control" id="categoryDetails" placeholder="categoryDetails" defaultValue={category.details}></textarea>
-        </div>
         <button type="submit" className="btn btn-default">Update</button>
-    </form>
+
+       </form>
+      </div>
     );
   }
 
-  PostsUpdate(e){
-    if(e && e.preventDefault){
-      e.preventDefault();
-    }
-    const {categoriesUpdate} = this.props;
-    const {name, details} = this.refs;
-    const {category_id} = this.props;
-    let formData = {
-        _id : category_id,
-      name : name.value,
-      details : details.value
-    };
-    categoriesUpdate(formData);
-
-  }
 }
 
 export default CategoriesUpdate;
