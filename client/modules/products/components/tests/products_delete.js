4b27fafb3f0ddcef81d4659184d2be680012cdc5
Ricardo Pumanes <ric@snapzio.com>
2016-05-23 16:05:18 +0800

    removed products_delete file

--- client/modules/products/components/tests/products_delete.js
+++
@@ -1,8 +0,0 @@
-const {describe, it} = global;
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-import ProductsDelete from '../products_delete';
-
-describe('products.components.products_delete', () => {
-  it('should do something');
-});
