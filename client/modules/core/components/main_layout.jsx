a601728e621647a81d40bee6ded0d460622bbd6c
jun <jun@snapzio.com>
2016-05-24 09:30:20 +0800

    updating issue on login

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -5,7 +5,9 @@ import NavBar from './nav_bar.jsx';
 
 class Layout extends React.Component {
   render() {
-    const {content} = this.props;
+    const {content,userId} = this.props;
+    console.log(userId);
+
     return (
       <div>
       <Helmet
@@ -20,7 +22,7 @@ class Layout extends React.Component {
           [ {name: 'viewport', content: 'width=device-width, initial-scale=1.0'} ]
         }
         script={[
-          {"src": "/bootstrap-3.3.6/js/bootstrap.min.js", "type": "text/javascript"}
+          {"src": "/bootstrap-3.3.6/js/bootstrap.min.js", "type": "text/javascript"},
         ]}
       />
         <div>
