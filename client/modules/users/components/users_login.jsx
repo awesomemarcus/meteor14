f37359612db92c80e2f3b63293ba0b31fc8b888d
jun <jun@snapzio.com>
2016-05-24 11:12:37 +0800

    change typo

--- client/modules/users/components/users_login.jsx
+++ client/modules/users/components/users_login.jsx
@@ -15,7 +15,7 @@ class UsersLogin extends React.Component{
         <label for="inputPassword" className="sr-only">Password</label>
         <input ref="password"  type="password" id="inputPassword" className="form-control" placeholder="Password" />
         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <a href="/users/signup" className="btn btn-lg btn-block btn-default">Signupt</a>
+        <a href="/users/signup" className="btn btn-lg btn-block btn-default">Signup</a>
       </form>
 
     </div>
