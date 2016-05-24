b794a0d0db638f1e4fca1f16b869ea44f279d454
jun <jun@snapzio.com>
2016-05-24 11:05:57 +0800

    added container for users

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 
-import MainLayout from './components/main_layout.jsx';
+import MainLayout from './containers/main_layout.js';
 import Home from './components/home.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
@@ -11,8 +11,8 @@ export default function (injectDeps, {FlowRouter}) {
     name: 'home',
     action() {
       mount(MainLayoutCtx, {
-        content: () => (<Home />)
+        content: () => (<Home />),
       });
-    }
+    },
   });
 }
--- client/modules/users/components/auth_checker.jsx
+++ client/modules/users/components/auth_checker.jsx
@@ -10,7 +10,7 @@ class AuthChecker extends React.Component{
         }
         return(
             <div>
-              <MainLayout  content={content}/>
+              <MainLayout  content={content} userId={userId} />
             </div>
         );
 
