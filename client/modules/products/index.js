e8f4fa37067143b230d17846a562a8c10d895db0
rod ghost <rod@snapzio.com>
2016-06-06 17:19:12 +0800

    loading stub

--- client/modules/products/index.js
+++ client/modules/products/index.js
@@ -1,8 +1,12 @@
 import actions from './actions';
 import routes from './routes';
+import stubs from './method_stubs';
 
 
 export default {
   routes,
   actions,
+  load(context){
+    stubs(context);
+  },
 };
