a5eec0d1a847139397f9e98e79a07fe091ac195e
Neil Anthony Te <websiteisfun@gmail.com>
2016-06-09 16:46:50 +0800

    changing parse int to float

--- client/modules/products/actions/products.js
+++ client/modules/products/actions/products.js
@@ -62,7 +62,7 @@ export default {
       return;
     }
 
-    Meteor.call("insertProduct", category_id, name, description, parseInt(price), function (err) {
+    Meteor.call("insertProduct", category_id, name, description, parseFloat(price), function (err) {
       if(err) {
         return LocalState.set('PRODUCTS_ADD_ERROR', err.error);
       }
