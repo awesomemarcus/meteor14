710ff16541e0aeb4a7bf105f002e5fd60b8f96ea
Mike <mike@snapzio.com>
2016-06-29 09:53:32 +0800

    Proto type

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
+/*eslint-disable no-unused-vars*/
+require('/.setting/local.js');
+/*eslint-enable no-unused-vars*/
+
 import publications from './publications';
 import methods from './methods';
+
+console.log(Meteor.settings.var1);
 
 publications();
 methods();
